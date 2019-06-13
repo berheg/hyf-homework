f75247d35a219fab6801ee465442a4e00180042f
Girmay <ink423103@yahoo.com>
2019-06-13 23:59:32 +0200

    renderProducts function added

--- javascript/javascript2/week1/hyf-bay/main.js
+++ javascript/javascript2/week1/hyf-bay/main.js
@@ -1,3 +1,41 @@
 console.log('Script loaded');
 
 console.log(getAvailableProducts());
+const products = getAvailableProducts();
+console.log(products) 
+function renderProducts(products){
+    const ul = document.querySelector("section.products > ul");
+    
+    for( let product of products){
+        const listName = document.createElement('li');
+        listName.innerHTML = product.name;
+        const listPrice = document.createElement('li');
+        listPrice.innerHTML = product.price;
+        const listRating = document.createElement('li');
+        listRating.innerHTML = product.rating; 
+        ul.appendChild(listName); 
+        ul.appendChild(listPrice); 
+        ul.appendChild(listRating); 
+        listShipsTo = document.createElement('li');
+        listShipsTo.innerHTML = product.shipsTo;     
+        const ul = document.createElement('ul')       
+        ul.appendChild(listShipsTo);
+    };
+};
+/*function createList(parent) {
+    const ul = document.createElement('ul')
+    const keys = Object.keys(parent);
+    for (const key of keys) {
+        const li = document.createElement('li');
+        if(Array.isArray(parent[key])){
+            li.appendChild(createList(parent[key]));
+        } else {
+            li.innerHTML = parent[key];
+            li.setAttribute('class', key)
+        }
+        ul.appendChild(li);
+
+    }
+    return ul;
+}*/
+    renderProducts(products);
