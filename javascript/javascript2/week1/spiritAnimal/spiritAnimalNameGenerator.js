ca05374bcc8deb5e51fa820a5033544ec5422766
Girmay <ink423103@yahoo.com>
2019-06-14 15:59:08 +0200

    event listener added not yet completed

--- javascript/javascript2/week1/spiritAnimal/spiritAnimalNameGenerator.js
+++ javascript/javascript2/week1/spiritAnimal/spiritAnimalNameGenerator.js
@@ -4,94 +4,64 @@ const spiritAnimal = ["The flying rat","The dancing mouse","cat","The barking do
 const btn = document.querySelector('button.btn');
 const inputBox = document.querySelector('input.userName');
 const result = document.querySelector('h1.result');
-const select = document.querySelector('select.selectEvent');
-btn.addEventListener('click',renderNewAnimalSpirit);
-inputBox.addEventListener('mouseover',renderNewAnimalSpirit);
-inputBox.addEventListener('change',renderNewAnimalSpirit);
-  function renderNewAnimalSpirit() {  
-        const randomIndex = parseInt(Math.random() * spiritAnimal.length);
-        let outPut = '' ;
-        if(inputBox.value === ""){
-            alert('Could you please insert your name and select one in the dropdown list!')    
-        }else{         
-            outPut =(inputBox.value + "-" + spiritAnimal[randomIndex]);     
-            
-        }
-        result.innerHTML = outPut;     
-        
-      };
-  /*function addListeners(){
-      if(window.addEventListener){
-          document.getElementById('btn').addEventListener('click',renderNewAnimalSpirit());
-          //document.getElementById('userName').addEventListener('onmouseenter',renderNewAnimalSpirit(),false);
-          //document.getElementById('userName').addEventListener('Mouseover',renderNewAnimalSpirit(),false);
-      }
-      window.onload = addListeners;
 
-  }*/
-  
-  /*function listenToSelectElement() {
-    const select = document.querySelector('select');
-  
-    select.addEventListener('change', selectchangeHandler)
-  
-  }  
-  
-  function selectchangeHandler() {
-    const select = document.querySelector('select');
-  
+function selectEventHandler(){
+    const select = document.querySelector('select.selectEvent');
     const selectedValue = select.value;
-  
-    if (selectedValue === 'buttonClick') {
-      listenToButtonClick();
-    } else if (selectedValue === 'hoverOnInput') {
-      listenToHover();
-    } else {
-      listenToTextchange();
-    }
-  }
-  
-  function listenToButtonClick() {   
-  
-    btn.addEventListener('click', buttonClickHandler);
-    input.removeEventListener('onmouseenter', listenToHover);
-    input.removeEventListener('keyup', textChangeHandler);
-  }
-  
-  function buttonClickHandler() {
-      alert('Could you please insert your name and select one in the dropdown list!');
-    result.innerHTML = renderNewAnimalSpirit(); 
+    select.addEventListener('change', function(){
+        if(selectedValue === 'buttonClick'){
+            inputBox.removeEventListener('onmouseenter', hoverListner());
+            inputBox.removeEventListener('change', inputChangeListener());
+            btn.addEventListener('click',()  =>{
+                renderNewAnimalSpirit; 
+            });            
+        }else if(selectedValue === 'hoverOnInput'){
+            inputBox.removeEventListener('change', inputChangeListener);
+            btn.removeEventListener('click',btnClickListener);
+            inputBox.addEventListener('mouseover',()  =>{
+                renderNewAnimalSpirit; 
+            });
+            
+        }else{
+            inputBox.removeEventListener('mouseover', hoverListner);
+            btn.removeEventListener('click',btnClickListener);
+            inputBox.addEventListener('change',()  =>{
+                renderNewAnimalSpirit; })          
+        } 
+    }) 
     
-  }
-  
-  function listenToHover() {
-    const btn = document.querySelector('button');
-    const input = document.querySelector('input');
-  
-    btn.removeEventListener('click', buttonClickHandler);
-    input.addEventListener('onmouseenter', listenToHover);
-    input.removeEventListener('keyup', textChangeHandler);
-  
-  }
-  function hoverHandler() {
+};
 
-    result.innerHTML = renderNewAnimalSpirit();
+function renderNewAnimalSpirit() {   
+    const randomIndex = parseInt(Math.random() * spiritAnimal.length);
+    let outPut = '' ;
+    if(inputBox.value === ""){
+        alert('\nCould you please insert your name and select one in the dropdown list!')
+           
+    }else{         
+        outPut =(inputBox.value + "-" + spiritAnimal[randomIndex]);     
+        
+    }
+    result.innerHTML = outPut;   
+    
+    };
+ 
+ function btnClickListener(){
+    inputBox.removeEventListener('change',inputChangeListener);
+    inputBox.removeEventListener('mouseover',hoverListner);
+    btn.addEventListener('click', renderNewAnimalSpirit)   
+ }
+ function inputChangeListener(){
+    inputBox.removeEventListener('mouseover',hoverListner);
+    btn.removeEventListener("click",btnClickListener);
+    inputBox.addEventListener('change', renderNewAnimalSpirit)
+ }         
+ 
+ function hoverListner(){
+   Btn.removeEventListener("click",btnClickListener);
+   inputBox.removeEventListener('change',inputChangeListener);   
+   inputBox.addEventListener("mouseover", renderNewAnimalSpirit);
+ 
+ }
+selectEventHandler();
   
-  }
-  
-  
-  function listenToTextchange () {
-    const btn = document.querySelector('button');
-    const input = document.querySelector('input');
-  
-    btn.removeEventListener('click', buttonClickHandler);
-    input.removeEventListener('onmouseenter', listenToHover);
-    input.addEventListener('keyup', textChangeHandler);
-  
-  }
-  function textChangeHandler() {
-    result.innerHTML = renderNewAnimalSpirit();  
-  }  
-  window.onload = listenToSelectElement();
-  listenToSelectElement();
-*/
