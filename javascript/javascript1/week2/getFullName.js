69cb24052b984f0797d66407aec130f1b2e28733
Girmay <ink423103@yahoo.com>
2019-06-16 08:07:48 +0200

    text removed

--- javascript/javascript1/week2/getFullName.js
+++ javascript/javascript1/week2/getFullName.js
@@ -1,20 +1,13 @@
-//Default constant text is assigned for parameters 
-const text = "no text given " 
 //getFullname function with three variables and default value
 function getFullname(firstname,surname,useFormalName){
     //fullname is assigned to the given parameters
     if(firstname === undefined || surname === undefined ){
-        return 'You have to set first name and sur name first!'
+        return 'You have to set name and sur name first!'
     }
     let fullname= firstname + ' ' + surname;
     //if the third parameter is true and not default value
-    if(useFormalName){
-        if(useFormalName!= text){
-        fullname = 'Lord ' + fullname;
-        }else if(useFormalName==text){
-        //if the third variable is the default value we manage it
-        fullname = text + fullname;
-        }
+    if(useFormalName){        
+        fullname = 'Lord ' + fullname;        
     }
     return fullname;
 };
