cfbaa7f4c71b5f13dc8c923d34f22b2b34a00737
Girmay <ink423103@yahoo.com>
2019-06-17 20:44:01 +0200

    first time added

---
+++ javascript/javascript2/week2/numberDoubling/numberDoubling.js
@@ -0,0 +1,23 @@
+//Doubles the odd numbers in an array and throws away the even number
+//numbers are declared to be used doen in the formula
+let numbers = [1, 2, 3, 4];
+let output = 'The given array: ['
+const par = document.querySelector('p.array');
+par.innerHTML = output + numbers + ']'
+//newNumbers are empty array to be populate double of odd numbers in numbers array
+let newNumbers = [];
+//filter is used with anonemous function to to filter the odd numbers from numbers array
+newNumbers = numbers.filter(function(num){
+    if(num%2===0){
+        return false;
+    }else{
+        return true;
+    }
+});
+output = 'The array after filtering only odds: ['
+const para1 = document.querySelector('p.para1');
+para1.innerHTML = output + newNumbers + ']';
+newNumbers = newNumbers.map(num => num*2);
+const para2 = document.querySelector('p.para2');
+para2.innerHTML = output + newNumbers +']';
+output = 'The array after filtering only odds and doubling: ['
