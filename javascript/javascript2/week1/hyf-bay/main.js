5ed36db058c651d531474970d4f079f6fd441fd8
Girmay <ink423103@yahoo.com>
2019-06-17 10:27:03 +0200

    function name creatListWithClassName changed to creatElementWithClassName

--- javascript/javascript2/week1/hyf-bay/main.js
+++ javascript/javascript2/week1/hyf-bay/main.js
@@ -2,19 +2,19 @@ console.log('Script loaded');
 
 console.log(getAvailableProducts());
 //forming list with the className
-function creatListWithClassName(tagName, className, text) {
-    const product = document.createElement(tagName);
-    product.className = className;
-    product.innerHTML = text;
-    return product;
+function creatElementWithClassName(tagName, className, text) {
+    const element = document.createElement(tagName);
+    element.className = className;
+    element.innerHTML = text;
+    return element;
 };
 
 // creat list inside ul
 function creatListsInsideUl(product) {
     const ul = document.createElement("ul");
-        ul.appendChild(creatListWithClassName('li', 'name', product.name)); 
-        ul.appendChild(creatListWithClassName('li', 'price', product.price));
-        ul.appendChild(creatListWithClassName('li', 'rating', product.rating));
+        ul.appendChild(creatElementWithClassName('li', 'name', product.name)); 
+        ul.appendChild(creatElementWithClassName('li', 'price', product.price));
+        ul.appendChild(creatElementWithClassName('li', 'rating', product.rating));
         ul.appendChild(creatUlShipping(product.shipsTo));
      return ul ;     
 };
@@ -22,7 +22,7 @@ function creatListsInsideUl(product) {
 function creatUlShipping(shipsTo) {
     const ul = document.createElement("ul");
         for (const item of shipsTo ){
-            ul.appendChild(creatListWithClassName('li', ' ', item));    
+            ul.appendChild(creatElementWithClassName('li', ' ', item));    
         }
     return ul;    
 };
