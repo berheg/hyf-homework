870fbdc30bbb99bbd67cefdb621cb9f67322b032
Girmay <ink423103@yahoo.com>
2019-06-18 08:42:02 +0200

    anonymous changed to arrow function

--- javascript/javascript2/week2/numberDoubling/numberDoubling.js
+++ javascript/javascript2/week2/numberDoubling/numberDoubling.js
@@ -7,7 +7,7 @@ par.innerHTML = output + numbers + ']'
 //newNumbers are empty array to be populate double of odd numbers in numbers array
 let newNumbers = [];
 //filter is used with anonemous function to to filter the odd numbers from numbers array
-newNumbers = numbers.filter(function(num){
+newNumbers = numbers.filter(num => {
     if(num%2===0){
         return false;
     }else{
@@ -18,6 +18,6 @@ output = 'The array after filtering only odds: ['
 const para1 = document.querySelector('p.para1');
 para1.innerHTML = output + newNumbers + ']';
 newNumbers = newNumbers.map(num => num*2);
+output = 'The array after filtering only odds and doubling: ['
 const para2 = document.querySelector('p.para2');
 para2.innerHTML = output + newNumbers +']';
-output = 'The array after filtering only odds and doubling: ['
