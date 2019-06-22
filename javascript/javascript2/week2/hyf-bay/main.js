0c6444667b31e24188427c3914616558ddd13453
Girmay <ink423103@yahoo.com>
2019-06-22 13:17:44 +0200

    input and select event listening add

--- javascript/javascript2/week2/hyf-bay/main.js
+++ javascript/javascript2/week2/hyf-bay/main.js
@@ -1,6 +1,9 @@
 console.log('Script loaded');
-
-const products = getAvailableProducts();
+//const products = getAvailableProducts();
+const availableProducts = getAvailableProducts();  
+const input = document.querySelector('input');
+const selectCountry = document.querySelector(".country > select");
+const selectSort = document.querySelector("div.sort select ");
 //forming element with the className
 function creatElementWithClassName(tagName, className, text) {
     const element = document.createElement(tagName);
@@ -8,7 +11,6 @@ function creatElementWithClassName(tagName, className, text) {
     element.innerHTML = text;
     return element;
 };
-
 // creat list inside ul
 function creatListsInsideUl(product) {
     const ul = document.createElement("ul");
@@ -34,33 +36,28 @@ function creatUlShippingCart(productList){
         ul.appendChild(li);
     }
 };
-
+const ul = document.querySelector("section.products > ul");
+const span = document.createElement('span.total'); 
 // creat list of product object with attributies name, price, rating and shipsTo and renders 
 function renderProducts (productList){
-    
     productList.sort();
     let total = 0;
     const ul = document.querySelector("section.products > ul");
     const span = document.createElement('span.total'); 
-    for (const product of productList)  {       
-        
-        const li = document.createElement("li");       
-        
+    for (const product of productList)  {    
+        const li = document.createElement("li");      
         li.appendChild(creatListsInsideUl(product)) ; 
         const btn = document.createElement('button');
         const cancelBtn = document.createElement('button')
-        
         span.innerHTML = total;   
         ul.appendChild(li);
         ul.appendChild(btn);
-        
         btn.innerHTML = 'Add to Cart'; 
         cancelBtn.innerHTML = 'Delet'
         const cart = document.querySelector('section.cart > ul');
         const listCart = document.createElement('li');
         const divCart = document.querySelector('div.total')
         btn.addEventListener('click',function(){
-            
             clearList(listCart);
             total += product.price;
             listCart.innerHTML = `
@@ -72,8 +69,7 @@ function renderProducts (productList){
             listCart.appendChild(cancelBtn);
             span.innerHTML = total;
             divCart.appendChild(span);
-            cart.appendChild(listCart);         
-                          
+            cart.appendChild(listCart);                
         });
         cancelBtn.addEventListener('click',function(e){
             total -= product.price; 
@@ -81,8 +77,7 @@ function renderProducts (productList){
             clearList(listCart);
             this.remove(li.childNodes);
             this.remove(ul.childNodes);
-        })          
-        
+        })     
     }
 };
 function clearList(parent){
@@ -90,22 +85,48 @@ function clearList(parent){
         parent.removeChild(parent.firstChild);
     }
 }
-const availableProducts = getAvailableProducts();  
-const input = document.querySelector('input');
-//const shipToDiv = document.querySelector('.country > div');
-//const shipToSelect = document.querySelector('.shipsTo > select');
 const sortOption = {
     Name: 'name',
     Expensive: 'expensive',
     Cheap: 'cheap'
 };
-input.addEventListener('input',function(){  
-    const productsToRender = availableProducts.filter(product =>{
-                productName =  product.name.toLowerCase();
-                inptutValue =  input.value.toLowerCase();     
-                return productName ===inptutValue;       
-            //if(shipToSelect.value === product.shipsTo.c )        
-    });        
+input.addEventListener('input',function(){ 
+    clearList(ul); 
+    const productsToRender = availableProducts.filter(function(product){
+        productName =  product.name.toLowerCase();
+        inptutValue =  input.value.toLowerCase();
+        if(productName === inptutValue)     
+            return true;                    
+    });           
 renderProducts(productsToRender);
 });
-renderProducts(products);
+// showing products that ships to country
+function searchByCountryName() {    
+    const selectedCountryName = selectCountry.value;
+    productToDisplay = availableProducts.filter(product => {
+      return product.shipsTo.some(shippingCountry => {
+        return shippingCountry.toLowerCase() === selectedCountryName.toLowerCase();
+      });
+    });
+    clearList(ul);
+    renderProducts(productToDisplay);
+  }
+selectCountry.addEventListener("change", searchByCountryName);
+selectSort.addEventListener("change",sortProducts);
+
+// sort the products
+function sortProducts() {
+  if (selectSort.value === "cheap") {
+    const sortCheapPrice = availableProducts.sort((a, b) => a.price - b.price);
+    renderProducts(sortCheapPrice);
+  }
+  if (selectSort.value === "expensive") {
+    const sortHighPrice = availableProducts.sort((a, b) => b.price - a.price);
+    renderProducts(sortHighPrice);
+  }
+  if (selectSort.value === "name") {
+    const sortName = availableProducts.sort((a, b) => (a.name > b.name ? 1 : -1));
+    renderProducts(sortName);
+  }
+}
+renderProducts(availableProducts);
