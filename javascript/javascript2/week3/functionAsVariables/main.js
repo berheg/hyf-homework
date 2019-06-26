9bad1f326235793be0c8072288171a4b410fbe03
Girmay <ink423103@yahoo.com>
2019-06-27 00:12:58 +0200

    first time added

---
+++ javascript/javascript2/week3/functionAsVariables/main.js
@@ -0,0 +1,27 @@
+const functionArray = [
+    add(),subtract(),multiply()
+]
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+/*for(let fun of functionArray){
+    console.log(fun(30.60));
+}*/
+const funConstant = function (){
+    return 'I am a constant function!'
+}
+function normalFunction(){
+    return 'I am normal function!'
+}
+console.log(funConstant());
+console.log(normalFunction());
+const funObject = {
+    add: (a,b) => {return a+b}
+}
+console.log(funObject.add(30,20));
