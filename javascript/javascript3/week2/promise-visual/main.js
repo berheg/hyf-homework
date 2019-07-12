1d8f8af682a87c24db8ff8e46a7def27018451b7
Girmay <ink423103@yahoo.com>
2019-07-12 14:25:00 +0200

    function used

--- javascript/javascript3/week2/promise-visual/main.js
+++ javascript/javascript3/week2/promise-visual/main.js
@@ -1,12 +1,19 @@
 
-for(let i = 20;i<=290; i +=10){
-    moveElement(redBox,{x:20,y:20+i});
+function redBallMove(){
+    for(let i = 20;i<=290; i +=10){
+        moveElement(redBox,{x:20,y:20+i});
+    }
 }
-for(let i = 20,j=20;i<=290,j<=390; i+=10 ,j +=10){
-    moveElement(blueBox,{x:20+j,y:20+i});
+function blueBallMove(){
+    for(let i = 20,j=20;i<=290,j<=390; i+=10 ,j +=10){
+        moveElement(blueBox,{x:20+j,y:20+i});
+    }
 }
-for(let i = 20;i<=390; i +=10){
-    moveElement(greenBox,{x:20+i,y:20});
+function greenBallMove(){
+    for(let i = 20;i<=390; i +=10){
+        moveElement(greenBox,{x:20+i,y:20});
+    }
 }
-
-
+redBallMove();
+blueBallMove();
+greenBallMove();
