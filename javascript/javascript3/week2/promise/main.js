18123a955d9d5fb31d5886ec20e6cd1badc95cc6
Girmay <ink423103@yahoo.com>
2019-07-12 14:11:05 +0200

    comment added

--- javascript/javascript3/week2/promise/main.js
+++ javascript/javascript3/week2/promise/main.js
@@ -1,3 +1,4 @@
+//Promise that resolves after  resolveAfter seconds
 function resolveAfter(resolveAfter){
     new Promise(function(resolve,reject){
         setTimeout(() => { resolve(console.log('I am called asynchronously after 6 seconds'));
