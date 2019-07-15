7a63a33ccdc82c57227170cf60cfd5dd3b1aa26d
Girmay <ink423103@yahoo.com>
2019-07-15 14:36:11 +0200

    getGeolocation added

--- javascript/javascript3/week2/promise/main.js
+++ javascript/javascript3/week2/promise/main.js
@@ -1,8 +1,39 @@
 //Promise that resolves after  resolveAfter seconds
-function resolveAfter(resolveAfter){
-    new Promise(function(resolve,reject){
-        setTimeout(() => { resolve(console.log('I am called asynchronously after 6 seconds'));
+function setTimeoutPromise(resolveAfter){
+    return new Promise(resolve => {
+        setTimeout(() => { resolve(`I am called asynchronously after ${resolveAfter} seconds`);
     },resolveAfter*1000)        
     })
-}
-resolveAfter(6);
+};
+setTimeoutPromise(3).then(console.log);
+setTimeoutPromise(6).then(console.log);
+//getCurrentLocation as promise
+function getCurrentLocation() {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const currentLocation = {
+            longitude: position.coords.longitude,
+            latitude: position.coords.latitude
+          };
+          resolve(currentLocation);
+        },
+        err => {
+          error = {
+            code: err.code,
+            message: err.message
+          };
+          reject(error);
+        }
+      );
+    });
+  }
+  getCurrentLocation()
+  .then(position => {
+    // called when the users position is found
+    console.log(position);
+  })
+  .catch(error => {
+    // called if there was an error getting the users location
+    console.log(error.message);
+  });
