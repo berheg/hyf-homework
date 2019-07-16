2fd7482a313384c9ad2de2aa094c4fb9d47a1fc4
Girmay <ink423103@yahoo.com>
2019-07-16 22:53:41 +0200

    after fetching data mapping added

--- javascript/javascript3/week2/promise/main.js
+++ javascript/javascript3/week2/promise/main.js
@@ -30,8 +30,15 @@ function getCurrentLocation() {
   }
   getCurrentLocation()
   .then(position => {
-    // called when the users position is found
+    // called when the users position is found    
     console.log(position);
+    const mymap = L.map('mymap').setView([position.latitude, position.longitude], 15);
+    const attribution =
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+    const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const tiles = L.tileLayer(tileUrl, { attribution });
+    tiles.addTo(mymap);
+    const marker = L.marker([position.latitude, position.longitude]).addTo(mymap);
   })
   .catch(error => {
     // called if there was an error getting the users location
