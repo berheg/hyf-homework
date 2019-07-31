22fd7140a9d6ccb7cf7427817de08a75d3475a52
Girmay <ink423103@yahoo.com>
2019-07-31 21:08:18 +0200

    circle drawn using function

--- javascript/javascript3/week3/canvas/main.js
+++ javascript/javascript3/week3/canvas/main.js
@@ -2,31 +2,54 @@ const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const c = canvas.getContext('2d');
-
-let x = 200;
-let y = 200;
-let dx = 4;
-let dy = 4;
-let radius = 30;
-c.fillStyle = "#3370d4"; //blue
-function animate(){
-    requestAnimationFrame(animate);
-    console.log('How long it loops');
-    c.clearRect(0, 0, innerWidth, innerHeight);
-    c.beginPath();
-    
-    c.arc(x, y, radius, 0, Math.PI*2, false);
+c.clearRect(0, 0, innerWidth, innerHeight);   
+//Draw Arc or Circle
+function drawCircle(){
+    let x =Math.random()*window.innerWidth;
+    let y = Math.random()* window.innerHeight;
+    c.beginPath(); 
+    c.fillStyle = "#3370d4"; //blue
+    c.arc(x, y, 30, 0, Math.PI*2, false);
     c.fill();
     c.strokeStyle = 'blue';
     c.stroke();
-    if(x + radius > innerWidth || x-radius < 0 ){
-        dx = -dx;
-    }
-    if(y + radius > innerHeight || y-radius < 0 ){
-        dy = -dy;
-    }
-    x +=dx;
-    y +=dy;
-
 }
-animate();
+
+/*class Circle{
+    constructor(x, y, r, startAngle, endAngle, fillColor){
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.startAngle = startAngle;
+        this.endAngle = endAngle;
+        this.fillColor = fillColor;
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        const c = canvas.getContext('2d');       
+        //c.clearRect(0, 0, innerWidth, innerHeight);
+        c.beginPath();
+        c.fillStyle = "#3370d4";
+        c.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle, false);
+        c.fill();
+        c.strokeStyle = this.fillColor;
+        c.stroke();        
+    }
+    draw(){
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        const c = canvas.getContext('2d');       
+        //c.clearRect(0, 0, innerWidth, innerHeight);
+        c.beginPath();
+        c.fillStyle = "#3370d4";
+        c.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle, false);
+        c.fill();
+        c.strokeStyle = this.fillColor;
+        c.stroke();
+    }   
+}
+
+const c1 = new Circle(50, 50, 20, 0, 2 * Math.PI, '#3370d4');*/
+//c1.draw;
+drawCircle();
+
+
