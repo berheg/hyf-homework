7cdc34352b7faaed086fcac353726c824ea18054
Girmay <ink423103@yahoo.com>
2019-08-01 23:09:02 +0200

    second circle class instance added

--- javascript/javascript3/week3/canvas/main.js
+++ javascript/javascript3/week3/canvas/main.js
@@ -2,7 +2,7 @@ const canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const c = canvas.getContext('2d');
-c.clearRect(0, 0, innerWidth, innerHeight);   
+//c.clearRect(0, 0, innerWidth, innerHeight);   
 //Draw Arc or Circle
 function drawCircle(){
     let x =Math.random()*window.innerWidth;
@@ -14,7 +14,6 @@ function drawCircle(){
     c.strokeStyle = 'blue';
     c.stroke();
 }
-
 class Circle{
     constructor(x, y, r, startAngle, endAngle, fillColor){
         this.x = x;
@@ -22,23 +21,9 @@ class Circle{
         this.r = r;
         this.startAngle = startAngle;
         this.endAngle = endAngle;
-        this.fillColor = fillColor;
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        const c = canvas.getContext('2d');       
-        //c.clearRect(0, 0, innerWidth, innerHeight);
-        c.beginPath();
-        c.fillStyle = "#3370d4";
-        c.arc(this.x, this.y, this.r, this.startAngle, this.endAngle, false);
-        c.fill();
-        c.strokeStyle = this.fillColor;
-        c.stroke();        
+        this.fillColor = fillColor;               
     }
-    draw(){
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        const c = canvas.getContext('2d');       
-        //c.clearRect(0, 0, innerWidth, innerHeight);
+    draw(){        
         c.beginPath();
         c.fillStyle = "#3370d4";
         c.arc(this.x, this.y, this.r, this.startAngle, this.endAngle, false);
@@ -47,9 +32,11 @@ class Circle{
         c.stroke();
     }   
 }
-
 const c1 = new Circle(50, 50, 20, 0, 2 * Math.PI, '#3370d4');
 c1.draw();
-/*drawCircle();
-let myCircle = window.setInterval(drawCircle(),500);*/
-
+let myC1 = window.setInterval(c1.draw(),500);
+let x =Math.random()*window.innerWidth;
+let y = Math.random()* window.innerHeight;
+const c2 = new Circle(x, y, 20, 0,2 * Math.PI, '#3370d4');
+c2.draw();
+window.setInterval(c2.draw(),500);
