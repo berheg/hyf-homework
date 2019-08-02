f5e788912d6edbb38fc8eb75df4dd3c6a57bfbf1
Girmay <ink423103@yahoo.com>
2019-08-02 21:55:58 +0200

    shopping cart list added

--- javascript/javascript3/week3/shoppingCart/main.js
+++ javascript/javascript3/week3/shoppingCart/main.js
@@ -1,10 +1,12 @@
+const addBtn = document.querySelector('button.addBtn');
+const searchBtn = document.querySelector('button.searchBtn');
 class Product {
     constructor(name, price) {
       this.name = name;
       this.price = price;
-    }
-  }
-  
+    }    
+  } 
+  let products = []
   class ShoppingCart {
     constructor(products) {
       this.products = products;
@@ -26,19 +28,20 @@ class Product {
       return this.products.filter((product) => {
         return product.name === productName.name;
       })
-    }
-  
+    }  
     getTotal() {
       // Implement functionality here
-      return this.products.reduce((total, product) => {
-        total += product.price;
-        return total;
-      }, 0);
+      let total = 0;
+    for (let product of this.products) {
+      const priceProduct = parseInt(product.price);
+      total += priceProduct;
     }
-    
+    return total;
+    }    
     renderProducts() {
       
-      function createList(parent, data) {
+      creatUlShippingCart(shoppingCart.products);
+      /*function createList(parent, data) {
         const list = document.createElement("li");
         list.innerHTML = data;
         parent.appendChild(list);
@@ -48,7 +51,7 @@ class Product {
       const productsList = document.querySelector("ul.productsList");
       this.products.forEach(product => {
         productsList.appendChild(createList(productsList, product.name));
-      });
+      });*/
     }
   
     getUser() {
@@ -57,13 +60,38 @@ class Product {
       .then(response => response.json())
     }
   } 
-  
+   
   const flatscreen = new Product("flat-screen", 5000);
   const flatscreen2 = new Product("flat-screen2", 5000);
+  const hpLaptop = new Product("hp-Laptop", 4000);
   
   const shoppingCart = new ShoppingCart([]);
   shoppingCart.addProduct(flatscreen);
-  shoppingCart.addProduct(flatscreen2);  
+  shoppingCart.addProduct(flatscreen2); 
+  shoppingCart.addProduct(hpLaptop);
+  console.log(shoppingCart.products); 
    
   shoppingCart.renderProducts();
-  
+  addBtn.addEventListener('click', addProduct);
+  searchBtn.addEventListener('click', addShoppingCart);
+  function addProduct(){
+    const inputProduct = document.querySelector('input#product');
+    if(inputProduct.innerHTML = ''){
+      alert("Please fill all three input")
+    }
+  }
+  function addShoppingCart(product){
+    const ul = document.querySelector("section.cart > ul");    
+        const li = document.createElement("li");
+        li.appendChild(creatListsInsideUl(product)) ;
+        ul.appendChild(li);
+    
+  }
+  function creatUlShippingCart(productList){
+    const ul = document.querySelector("section.cart > ul");
+    for (const product of productList)  {
+        const li = document.createElement("li");
+        li.innerHTML = product.name               
+        ul.appendChild(li);
+    }
+};
