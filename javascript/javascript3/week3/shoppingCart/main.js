73dabcd399d21bb36b9d071da763a501e16aca00
Girmay <ink423103@yahoo.com>
2019-08-03 12:19:42 +0200

    add and delet button faactionality added

--- javascript/javascript3/week3/shoppingCart/main.js
+++ javascript/javascript3/week3/shoppingCart/main.js
@@ -1,12 +1,14 @@
 const addBtn = document.querySelector('button.addBtn');
 const searchBtn = document.querySelector('button.searchBtn');
+const usernameP = document.querySelector('#username')
+let username;
 class Product {
-  constructor(name, price) {
+  constructor(name, price, description) {
     this.name = name;
     this.price = price;
+    this.description = description;
   }    
 } 
-let products = []
 class ShoppingCart {
   constructor(products) {
     this.products = products;
@@ -30,62 +32,74 @@ class ShoppingCart {
     })
   }  
   getTotal() {
-    // Implement functionality here
-    let total = 0;
+    // Implement functionality here    
     for (let product of this.products) {
       const priceProduct = parseInt(product.price);
       total += priceProduct;
     }
     return total;
   }    
-  renderProducts() {
-    
-    //creatUlShippingCart(shoppingCart.products);
-    /*function createList(parent, data) {
-      const list = document.createElement("li");
-      list.innerHTML = data;
-      parent.appendChild(list);
-      return list;
-    }
-
-    const productsList = document.querySelector("ul.productsList");
-    this.products.forEach(product => {
-      productsList.appendChild(createList(productsList, product.name));
-    });*/
+  renderProducts() {    
+    let total = 0;
     const ul = document.querySelector("ul.productsList");
-    //const span = document.createElement('span.total'); 
+    const span = document.createElement('span.total'); 
     for (let product of this.products)  {    
-        const li = document.createElement("li");  
-        li.innerHTML = product.name;    
-        //li.appendChild(li); 
-        const btn = document.createElement('button');
-        const cancelBtn = document.createElement('button');
-        console.log(product.name);
-        //span.innerHTML = total;   
-        ul.appendChild(li);
-        ul.appendChild(btn);
-        btn.innerHTML = 'Add to Cart'; 
-        cancelBtn.innerHTML = 'Delet'       
-        btn.addEventListener('click',function(){
-          //clearList(listCart);
-          //total += product.price;
-          listCart.innerHTML = `
-              <li>
-                  <div class="name">${product.name}</div>
-                  <div class="price">${product.price}</div>
-              </li>
-          `;
-          listCart.appendChild(cancelBtn);
-          //span.innerHTML = total;
-          //divCart.appendChild(span);
-          cart.appendChild(listCart);                
-        });
-    }
-};
+      const li = document.createElement("li");  
+      li.innerHTML = product.name;  
+      li.appendChild(creatListsInsideUl(product));         
+      const btn = document.createElement('button');
+      const cancelBtn = document.createElement('button');
+      console.log(product.name);
+      span.innerHTML = total;   
+      ul.appendChild(li);
+      ul.appendChild(btn);
+      btn.innerHTML = 'Add to Cart'; 
+      cancelBtn.innerHTML = 'Delet' 
+      const cart = document.querySelector('section.cart > ul');
+      const listCart = document.createElement('li');
+      const divCart = document.querySelector('div.total');
+      let userChecker = false;     
+      btn.addEventListener('click',function(){
+        clearList(listCart);
+        if(!userChecker){
+          usernameP.innerHTML += username;
+          userChecker = true;
+        }
+        total += product.price;
+        listCart.innerHTML = `
+            <li>
+                <div class="name">${product.name}</div>
+                <div class="price">${product.price}</div>
+            </li>
+        `;
+        listCart.appendChild(cancelBtn);
+        span.innerHTML = total;
+        divCart.appendChild(span);
+        cart.appendChild(listCart);                
+      });
+      cancelBtn.addEventListener('click',function(e){
+        total -= product.price; 
+        span.innerHTML = total;           
+        clearList(listCart);
+        this.remove(li.childNodes);
+        this.remove(ul.childNodes);
+      });
+    };
+  };
   getUser() {
-    // Implement functionality here
-    return fetch(`https://jsonplaceholder.typicode.com/users/1`)
+    // Implement functionality here    
+    const min = 1;
+    const max = 10;
+    const userNumber = Math.floor (Math.random () * (max - min)) + min;
+    console.log(userNumber);
+    fetch(`https://jsonplaceholder.typicode.com/users/${userNumber}`)
     .then(response => response.json())
+    .then(json =>{username = json.username;
+    console.log(username)} )
+    .catch(error => {
+      console.log (error);
+      usernameP.innerHTML = 'No';
+    });
   }
 } 
 function clearList(parent){
@@ -94,16 +108,17 @@ function clearList(parent){
   }
 }
    
-const flatscreen = new Product("flat-screen", 5000);
-const flatscreen2 = new Product("flat-screen2", 5000);
-const hpLaptop = new Product("hp-Laptop", 4000);
+const flatscreen = new Product("flat-screen", 5000, 'Samsung 49" QLED smart ');
+const flatscreen2 = new Product("flat-screen2", 5000, 'Samsung TV UHD LED 65" ');
+const hpLaptop = new Product("hp-Laptop", 4000, 'HP 14-dk0002no 14" ');
 
 const shoppingCart = new ShoppingCart([]);
 shoppingCart.addProduct(flatscreen);
 shoppingCart.addProduct(flatscreen2); 
 shoppingCart.addProduct(hpLaptop);
 console.log(shoppingCart.products); 
-
+shoppingCart.getUser();
+console.log(username);
 shoppingCart.renderProducts();
 addBtn.addEventListener('click', addProduct);
 searchBtn.addEventListener('click', addShoppingCart);
@@ -113,6 +128,21 @@ function addProduct(){
     alert("Please fill all three input")
   }
 }
+//forming element with the className
+function creatElementWithClassName(tagName, className, text) {
+  const element = document.createElement(tagName);
+  element.className = className;
+  element.innerHTML = text;
+  return element;
+};
+// creat list inside ul
+function creatListsInsideUl(product) {
+  const ul = document.createElement("ul");
+      ul.appendChild(creatElementWithClassName('li', 'name', product.name)); 
+      ul.appendChild(creatElementWithClassName('li', 'price', product.price));
+      ul.appendChild(creatElementWithClassName('li', 'rating', product.description));
+  return ul ;     
+};
 function addShoppingCart(product){
   const ul = document.querySelector("section.cart > ul");    
     const li = document.createElement("li");
@@ -127,14 +157,3 @@ function creatUlShippingCart(productList){
     ul.appendChild(li);
   }
 };
-const cart = document.querySelector('section.cart > ul');
-const listCart = document.createElement('li');
-const divCart = document.querySelector('div.total')
-/*
-cancelBtn.addEventListener('click',function(e){
-  total -= product.price; 
-  span.innerHTML = total;           
-  clearList(listCart);
-  this.remove(li.childNodes);
-  this.remove(ul.childNodes);
-})*/     
