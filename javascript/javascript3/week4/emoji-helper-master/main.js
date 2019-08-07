9a0b7e3851d307da5efa7d343ba1b7b076746413
Girmay <ink423103@yahoo.com>
2019-08-07 19:49:33 +0200

    renderHTML and searchEmoji modified

--- javascript/javascript3/week4/emoji-helper-master/main.js
+++ javascript/javascript3/week4/emoji-helper-master/main.js
@@ -1,6 +1,7 @@
 console.log("Script loaded");
 const searchField = document.getElementById("searchField");
-
+const ulTag = document.querySelector("ul.container");
+const ulFavoriteList = document.querySelector(' ul.favoriteList')
 let listOfEmojis;
 function fetchEmojis(){
 	fetch("https://raw.githubusercontent.com/amio/emoji.json/master/emoji.json")
@@ -12,14 +13,11 @@ function fetchEmojis(){
 	})
 }
 
-function renderHTML(listOfEmojis){
-	const ulTag = document.querySelector("ul");
-	ulTag.innerHTML = ""
+function renderHTML(listOfEmojis, ulList = ulTag){
+	
+	ulList.innerHTML = ""
 	listOfEmojis.forEach((emoji) => {
 		const liTag = document.createElement("li");
-		// liTag.innterHTML = emoji.char
-
-		//
 		const emojiSpan = document.createElement("span");
 		emojiSpan.innerHTML = emoji.char
 		emojiSpan.classList.add("emoji");
@@ -29,20 +27,20 @@ function renderHTML(listOfEmojis){
 		nameSpan.innerHTML = emoji.name
 		nameSpan.classList.add("emojiName");		
 		liTag.appendChild(nameSpan);
-		//
+		//save to clipboard
+		liTag.addEventListener('click', () => {
+			writeToClipboard(emoji.char);
+			saveToFavorite(emoji);
+		})
 
-		ulTag.appendChild(liTag);
+		ulList.appendChild(liTag);
 	})
 }
-
-fetchEmojis();
-
-searchField.addEventListener("keyup",() => 
-	searchEmoji(searchField.value));
-function searchEmoji(searchValue){
+searchField.addEventListener("keyup",() => searchEmoji(searchField.value,'name'));
+function searchEmoji(searchValue, searchOption){
     const newListOfEmojis = listOfEmojis.filter((emoji) => {
-		return emoji.name.toLowerCase().includes(searchValue.toLowerCase());
+		return emoji[searchOption].toLowerCase().includes(searchValue.toLowerCase());
 	});
 	renderHTML(newListOfEmojis);
 }
-
+fetchEmojis();
