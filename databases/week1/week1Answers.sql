156e79eb9e888405a93057d3bde32c6797f7aab8
Girmay Hailu Berhe <48474199+berheg@users.noreply.github.com>
2019-08-16 23:12:08 +0200

    OR used instead

--- databases/week1/week1Answers.sql
+++ databases/week1/week1Answers.sql
@@ -1,9 +1,10 @@
 1. Find out how many tasks are in the task table
-	SELECT COUNT(id) AS totalTasks FROM task;
+	SELECT COUNT(id) AS totalTasks 
+	FROM task;
 
 2. Find out how many tasks in the task table do not have a valid due date
 	SELECT COUNT(id) AS tasksNullDueDate 
-    FROM task 
+    	FROM task 
 	WHERE due_date IS NULL;
 
 3. Find all the tasks that are marked as done
@@ -14,34 +15,34 @@
 
 4.Find all the tasks that are not marked as done
 	SELECT task.id, task.title, status.name 
-    FROM task 
+    	FROM task 
 	INNER JOIN status ON task.status_id = status.id
 	WHERE status.name not Like 'Done';
 
 5.Get all the tasks, sorted with the most recently created first
 	SELECT * 
-    FROM task 
+    	FROM task 
 	ORDER BY created DESC;
 
 6.Get the single most recently created task
 	SELECT * 
-    FROM task 
-    ORDER BY created DESC 
-    limit 1;
+   	FROM task 
+    	ORDER BY created DESC 
+    	limit 1;
 
 7.Get the title and due date of all tasks where the title or description contains database
 	SELECT title, due_date 
-    FROM task 
-	WHERE title LIKE '%database%' || description LIKE '%database%';
+    	FROM task 
+	WHERE title LIKE '%database%' OR description LIKE '%database%';
 
 8.Get the title and status (as text) of all tasks
 	SELECT task.title, status.name  
-    FROM task 
+    	FROM task 
 	left JOIN status ON task.status_id = status.id;
 
 9.Get the name of each status, along with a count of how many tasks have that status
 	SELECT status.name as statusName, count(task.status_id) As countOfTasks 
-    FROM task 
+    	FROM task 
 	Left JOIN status ON task.status_id = status.id
 	GROUP BY statusName;
 
