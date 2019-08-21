9d8ac9a522e89d71793eabef761585e85a8f2729
Girmay <ink423103@yahoo.com>
2019-08-21 22:13:17 +0200

    init commit

---
+++ databases/week2/nodeJS/app.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const mysql = require('mysql');
+
+// Create connection
+const db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '123456',
+    database : 'hyf19'    
+});
+
+// Connect
+db.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySql Connected...');
+});
+
+const app = express();
+const addNewTask = function(title, description, created, updated, dueDate, statusID, userID) {
+    const sql = `insert into task ` +
+                `(title, description, created, updated, due_date, status_id, user_id) ` +
+                `values ('${title}', '${description}', '${created}', '${updated}', '${due_date}', '${status_id}', '${user_id}')` ;
+     
+   return sql;
+ };
+ 
+ const changeTaskTitle = function(taskID, newTitle) {
+     const sql = `update task ` +
+                 `set title = '${newTitle}'` + 
+                 `where id = '${taskID}'`;
+     return sql;
+ };
+ 
+ const changeTaskDueDate = function(taskID, newDueDate) {
+     const sql = `update task ` + 
+                 `set due_date = '${newDueDate}'` + 
+                 `where id = '${taskID}'`;
+     return sql;
+   
+ };
+ 
+ const changeTaskStatus = function(taskID, newStatus) {
+     const sql = `update task inner join status on status.id = tassk.status_id ` + 
+                 `set task.status_id = status.id ` + 
+                 `where status.name = '${newStatus}' and task.id = '${taskID}'`;
+     return sql;
+ };
+ 
+ const markTaskAsCompleted = function(taskID) {
+     const sql = `update task inner join status on status.id = tassk.status_id ` + 
+                 `set task.status_id = status.id ` + 
+                 `where status.name = 'Done' and task.id = '${taskID}'`;
+     return sql;
+ };
+ 
+ const deleteTask = function(taskID) {
+     const sql = `delete from task where id = '${taskID}'`;
+     return sql;
+ };
+ function sqlQuery(queryToBeExcuted){
+     let query = db.query(queryToBeExcuted, (err, result) => {
+         if(err) throw err;
+         console.log(result);
+         res.send(`Post inserted...`);
+     });
+     return query
+ }
+ app.get('/updatepost/:id', (req, res) => {
+     sqlQuery(deleteTask(2));
+ });
+ app.listen('3000', () => {
+    console.log('Server started on port 3000');
+});
