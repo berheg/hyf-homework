a8f5273b03ea16970d5e73003c9c1c4e0fcfca8a
Girmay <ink423103@yahoo.com>
2019-08-21 11:04:52 +0200

    comment modified

--- databases/week2/schoolDatabase.sql
+++ databases/week2/schoolDatabase.sql
@@ -1,10 +1,7 @@
-    
--- Create a new database containing the following tables:
--- Class: with the columns: id, name, begins (date), ends (date)
--- Student: with the columns: id, name, email, phone, class_id (foreign key)
+-- Create a new database school
 create database school;
 use school;
--- CREATE TABLE [table] ([column] VARCHAR(120), [another-column] DATETIME);
+-- CREATE TABLE Class: with the columns: id, name, begins (date), ends (date)
 create table `class` (
 `id` int(10) unsigned not null auto_increment,
 `name` varchar(180) not null,
@@ -12,7 +9,7 @@ create table `class` (
 `ends (date)` date not null,
 primary key (`id`)
 )  engine=innodb default charset=utf8mb4 collate=utf8mb4_unicode_ci;
-
+-- CREATE TABLE Student: with the columns: id, name, email, phone, class_id (foreign key)
 create table `student` (
 `id` int(10) unsigned not null auto_increment,
 `name` varchar(255) not null,
@@ -25,7 +22,7 @@ constraint `fk_class_id` foreign key (`class_id`) references `class` (`id`) on d
 engine=innodb default charset=utf8mb4 collate=utf8mb4_unicode_ci;
 
 -- Create an index on the name column of the student table.
+use school;
 create index name_index on student(name);
-
 -- Add a new column to the class table named status which can only have the following values: not-started, ongoing, finished (hint: enumerations).
 alter table class add column status enum('notstarted', 'ongoing', 'finished');
