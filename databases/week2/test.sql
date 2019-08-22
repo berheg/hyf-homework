0d9a76d0190ddac153881b0999e43c16396f075a
Girmay <ink423103@yahoo.com>
2019-08-22 22:29:22 +0200

    inner join condition modified

--- databases/week2/test.sql
+++ databases/week2/test.sql
@@ -2,15 +2,14 @@ SELECT * FROM hyf19.task;
 use hyf19;
 update task
 	inner join 
-		status on   task.status_id = status.id     
+		status on status.name = 'Done'     
 	set task.status_id = status.id
-	where status.name = 'Done' and
-		  task.id = 1;
+	where task.id = 1;
 update status
 	set status.name = 'In progress'
 		where id = 2;
 update task
-	inner join 
-		status on status.id = task.status_id
-	set task.status_id = status.id 
-		where status.name = '${newStatus}' and task.id = '${taskID}';
+	inner join
+		status on status.name = 'In progress'
+	set task.status_id = status.id		
+		where task.id = 2;
