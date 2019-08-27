7bd81b12c6d8f5d5be81ee1ef22c79836dd6d250
Girmay <ink423103@yahoo.com>
2019-08-27 22:58:46 +0200

    addNewArticle function add

--- databases/week3/NodeJs/app.js
+++ databases/week3/NodeJs/app.js
@@ -13,7 +13,7 @@ const db = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : '123456',
-    database : 'hyf19'    
+    database : 'newsp'    
 });
 // Connect
 db.connect((err) => {
@@ -24,3 +24,10 @@ db.connect((err) => {
 });
 
 const app = express();
+//function returns sql query for adding new article row in newsp database
+const addNewArticle = function(title, artical_text, date) {
+    const sql = `insert into article ` +
+                `(title, artical_text, date) ` +
+                `values ('${title}', '${artical_text}', '${date}')` ;     
+   return sql;
+ };
