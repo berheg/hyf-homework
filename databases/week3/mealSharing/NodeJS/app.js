02f4d9a09b6e045e91a7f8e45bfa967f93ed2a79
Girmay <ink423103@yahoo.com>
2019-08-30 09:20:10 +0200

    Router for getting meal table added

--- databases/week3/mealSharing/NodeJS/app.js
+++ databases/week3/mealSharing/NodeJS/app.js
@@ -27,21 +27,30 @@ const app = express();
 //function returns sql query for adding new meal row in mealsharing database
 const addNewMeal = function(title, description,location, when, date) {
     const sql = `insert into meal ` +
-                `(title, artical_text, date) ` +
+                `(title, description, location,when,date) ` +
                 `values ('${title}', '${description}', '${location}','${when}','${date}')` ;     
    return sql;
  };
  //function returns sql query for adding new reservation row in mealsharing database
 const addNewReservation = function(number_of_guests, meal_id, created_date) {
-    const sql = `insert into meal ` +
-                `(title, artical_text, date) ` +
+    const sql = `insert into reservation ` +
+                `(number_of_guests, artical_text, created_date) ` +
                 `values (${number_of_guests}, ${meal_id},'${created_date}')` ;     
    return sql;
  };
   //function returns sql query for adding new review row in mealsharing database
 const addNewReview = function(title, description, review_meal_id, stars, created_date) {
-    const sql = `insert into meal ` +
-                `(title, artical_text, date) ` +
+    const sql = `insert into review ` +
+                `(title, description, review_meal_id, stars, created_date) ` +
                 `values ('${title}','${description}', ${review_meal_id},'${stars}','${created_date}')` ;     
    return sql;
  };
+ // Router for getting meal table
+ app.get('/getmeal', (req, res) => {
+    let sql = 'SELECT * FROM meal';
+    let query = db.query(sql, (err, results) => {
+        if(err) throw err;
+        console.log(results);
+        res.send('Meal fetched...');
+    });
+});
