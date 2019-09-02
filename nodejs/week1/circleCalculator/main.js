86adbbe245599fc040569ce2bfdc67915db1265c
Girmay <ink423103@yahoo.com>
2019-09-02 22:33:09 +0200

    comment added

--- nodejs/week1/circleCalculator/main.js
+++ nodejs/week1/circleCalculator/main.js
@@ -1,16 +1,24 @@
+/*Circle class
+ * It consists of constructor , getdiameter, getCircumference and getArea methods
+ * @param radius 
+ */
 class Circle{
+    //constructor for circle class
     constructor(radius){
         this.radius = radius;
+        //getDiameter method returens diameter of the circle with given radius
         this.getDiameter= function(){
             const diameter = 2*this.radius;
             console.log('Diameter: ' + diameter);
             return diameter;
         }
+        //getCircumference method returens circumference of the circle with given radius
         this.getCircumference = function(){
             const circumference = 2*  Math.PI * this.radius;
             console.log('Circumference: '+ circumference);
             return circumference;
         }
+        //getArea method returens area of the circle with given radius
         this.getArea = function(){
             const area =  Math.PI * this.radius*this.radius;
             console.log('Area: ' + area);
