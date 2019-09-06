1ad50962b3602b00afcc0f589cd01fb209a1722f
Girmay <ink423103@yahoo.com>
2019-09-06 15:35:24 +0200

    added as router

---
+++ nodejs/week1/mealSharing/src/backend/routes/selectedreservations.js
@@ -0,0 +1,22 @@
+const express = require ('express');
+const router = express.Router ();
+const fs = require ('fs');
+const reservationsJson = fs.readFileSync (
+    __dirname + '/../data/reservation.json',
+    'utf8'
+  );
+  const reservationsObject = JSON.parse (reservationsJson);
+//Router for reservations with mealId request
+router.get('/:id', (req, res) => {    
+    const selectedReservation = reservationsObject.filter(reservation => 
+        {return reservation.mealId = parseInt(req.params.id);});    
+    if(selectedReservation.length == 1)    
+        res.send(selectedReservation[0]);
+    else{
+        res.status(404)//Set status to 404 as movie was not found
+        res.send({message: "Not Found"});
+    }
+}
+);
+//Router will go here
+module.exports=router;
