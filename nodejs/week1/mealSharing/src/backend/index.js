a7bae9eef26b64f1c95b99ece65498ffc5a3bfe6
Girmay <ink423103@yahoo.com>
2019-09-06 15:33:26 +0200

    Routers moved and it is middleware

--- nodejs/week1/mealSharing/src/backend/index.js
+++ nodejs/week1/mealSharing/src/backend/index.js
@@ -1,53 +1,31 @@
-
+//Import required Routers in the given directory
 const express = require('express');
+const mealsRoutes = require( __dirname +'/routes/meals.js');
+const cheapMealsRoutes = require (__dirname +'/routes/cheap-Meals.js');
+const largeMealsRoutes = require (__dirname +'/routes/large-Meals.js');
+const selectedMealsRoutes = require (__dirname +'/routes/selectedMeals.js');
+const reservationsRoutes = require( __dirname +'/routes/reservations.js');
+const selectedReservationsRoutes = require( __dirname +'/routes/selectedreservations.js');
 const app = express();
-const fs = require ('fs');
-const mealsJson = fs.readFileSync (
-    __dirname + '/data/meals.json',
-    'utf8'
-  );
-  const reservationsJson = fs.readFileSync (
-    __dirname + '/data/reservation.json',
-    'utf8'
-  );
-const mealsObject = JSON.parse (mealsJson);
-const reservationsObject = JSON.parse (reservationsJson);
-//Server listening
+//Server listening function
 app.listen(3000, () => console.log('Listening at 3000'));
-//Router for meals request
-app.get('/meals', (req, res) => {    
-      
-        res.send(mealsObject);
-    }
-);
-//Router for cheap-meals request
-app.get('/cheap-meals', (req, res) => {    
-        const cheapMealsObject = mealsObject.filter(meal => {return meal.price <100;});
-        res.send(cheapMealsObject);
-    }
-);
-//Router for large-meals request
-app.get('/large-meals', (req, res) => {    
-    const lageMealsObject = mealsObject.filter(meal => {return meal.maxNumberOfGuests > 10;});
-    res.send(lageMealsObject);
-}
-);
-//Router for meal id request
-app.get('/meal/:id', (req, res) => {    
-    const selectedMeal = mealsObject.filter(meal => {return meal.id = parseInt(req.params.id);});
-    res.send(selectedMeal);
-}
-);
-//Router for reservations request
-app.get('/reservations', (req, res) => {      
-    res.send(reservationsObject);
-}
-);
-//Router for reservations mealId request
-app.get('/reservation/:id', (req, res) => {    
-    const selectedReservation = reservationsObject.filter(reservation => {return reservation.mealId = parseInt(req.params.id);});
-    res.send(selectedReservation);
-}
-);
+//Middlewares call appropriate router to use based on the request route
+app.use('/meals', mealsRoutes);
+app.use('/cheap-meals', cheapMealsRoutes);
+app.use('/large-meals',largeMealsRoutes);
+app.use('/meal',selectedMealsRoutes);
+app.use('/reservations', reservationsRoutes);
+app.use('/reservation/', selectedReservationsRoutes);
+
+
+
+
+
+
+
+
+
+
+
 
 
