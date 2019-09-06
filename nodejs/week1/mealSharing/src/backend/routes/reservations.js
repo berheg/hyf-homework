394a5143e83c97956003e227e866654e506a0d91
Girmay <ink423103@yahoo.com>
2019-09-06 15:37:20 +0200

    added as router

---
+++ nodejs/week1/mealSharing/src/backend/routes/reservations.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const router = express.Router ();
+const fs = require ('fs');
+const reservationsJson = fs.readFileSync (
+    __dirname + '/../data/reservation.json',
+    'utf8'
+  );
+  const reservationsObject = JSON.parse (reservationsJson);
+//Router for reservations request
+router.get('/', (req, res) => {    
+    res.send(reservationsObject);
+}
+);
+module.exports=router;
