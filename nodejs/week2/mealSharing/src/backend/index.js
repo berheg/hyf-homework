f5d570481788f64cabf24a64b727de0a1977cc90
Girmay <ink423103@yahoo.com>
2019-09-13 22:01:02 +0200

    middlewares and routers added

--- nodejs/week2/mealSharing/src/backend/index.js
+++ nodejs/week2/mealSharing/src/backend/index.js
@@ -1,18 +1,26 @@
 //Import required Routers in the given directory
 const express = require('express');
 const mealsRoutes = require( __dirname +'/routes/meals.js');
-const cheapMealsRoutes = require (__dirname +'/routes/cheap-Meals.js');
-const largeMealsRoutes = require (__dirname +'/routes/large-Meals.js');
-const selectedMealsRoutes = require (__dirname +'/routes/selectedMeals.js');
+const bodyParser = require('body-parser');
+
+const selectedMealRoutes = require (__dirname +'/routes/selectedMeal.js');
 const reservationsRoutes = require( __dirname +'/routes/reservations.js');
-const selectedReservationsRoutes = require( __dirname +'/routes/selectedreservations.js');
+const selectedReservationRoutes = require( __dirname +'/routes/selectedReservation.js');
+const reviewsRoutes = require( __dirname +'/routes/reviews.js');
+const selectedReviewRoutes = require( __dirname +'/routes/selectedReview.js');
 const app = express();
+app.use(bodyParser());
+
+//Middlewares call appropriate router to use based on the request route
+app.use((req,query,next) =>{
+    console.log(`${new Date()} request received for path: ${req.url}`);
+    next();
+})
+app.use('/meals', mealsRoutes);
+app.use('/meals/',selectedMealRoutes);
+app.use('/reservations', reservationsRoutes);
+app.use('/reservationS/', selectedReservationRoutes);
+app.use('/reviews', reviewsRoutes);
+app.use('/reviews/', selectedReviewRoutes);
 //Server listening function
 app.listen(3000, () => console.log('Listening at 3000'));
-//Middlewares call appropriate router to use based on the request route
-app.use('/meals', mealsRoutes);
-app.use('/cheap-meals', cheapMealsRoutes);
-app.use('/large-meals',largeMealsRoutes);
-app.use('/meal',selectedMealsRoutes);
-app.use('/reservations', reservationsRoutes);
-app.use('/reservation/', selectedReservationsRoutes);
