ab842f51b2289c6161a77a47a060d618b75e125b
Girmay <ink423103@yahoo.com>
2019-09-18 23:39:10 +0200

    init commit

---
+++ nodejs/week3/Calculator/ReqBody/routes/subtraction.js
@@ -0,0 +1,34 @@
+
+//numChecker validates weather or not the given strings are numbers
+const numCheCker = require('./numChecker.js');
+function subtraction(firstNum,secondNum){
+    let sumFirstNum = 0; 
+    let sumSecondNum = 0;
+    if(typeof(firstNum)!=="object"){
+        sumFirstNum = parseInt(firstNum);
+    }else{
+        for(let j=0; j<firstNum.length;j++){           
+            if(numCheCker(firstNum[j])===true){ 
+                sumFirstNum += parseInt(firstNum[j]);            
+            }else
+                return numCheCker(firstNum[j]);
+        } 
+    }
+    console.log(typeof(secondNum));
+    if(typeof(secondNum)!== "object"){
+        sumSecondNum = parseInt(secondNum);
+    }else{
+        for(let i=0; i < secondNum.length;i++){
+            if(numCheCker(secondNum[i])===true){             
+                sumSecondNum += parseInt(secondNum[i]);
+            }else
+                return numCheCker(secondNum[i]);
+        } 
+    }
+    const subtraction = sumFirstNum-sumSecondNum;     
+    return subtraction; 
+
+}
+
+//Router will go here
+module.exports=subtraction;
