bb8851eb92043b059fa0454829a0add22b277f5c
Girmay <ink423103@yahoo.com>
2019-09-22 20:11:26 +0200

    dotenv file used

--- nodejs/week3/homework/src/backend/database.js
+++ nodejs/week3/homework/src/backend/database.js
@@ -11,7 +11,7 @@ const pool = mysql.createPool({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   port: process.env.DB_PORT,
-  multipleStatements: true,      
+  multipleStatements: true      
 });
 
 pool.getConnection((err, connection) => {
