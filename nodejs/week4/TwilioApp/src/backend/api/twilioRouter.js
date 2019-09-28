153a519c0eb4e6252e798f4f65e90987de50e772
Girmay <ink423103@yahoo.com>
2019-09-28 23:25:06 +0200

    if conditions modified

--- nodejs/week4/TwilioApp/src/backend/api/twilioRouter.js
+++ nodejs/week4/TwilioApp/src/backend/api/twilioRouter.js
@@ -18,7 +18,7 @@ const bodyParserUrlencoded = bodyParser.urlencoded({ extended: true });
 // c. receive sms
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 router.get("/", (req, res) => {
-    const incomingMessage = req.body.msg;   
+    const incomingMessage = req.body;   
     let msgToSend; 
     if(incomingMessage === 'help'){
         msgToSend = 'Send  menu: to get lists of avialiable meals types' + 
@@ -28,8 +28,9 @@ router.get("/", (req, res) => {
     else if(incomingMessage === 'menu'){
         msgToSend = '1:pizza 2:burger 3:salad';
     }
-    else if(incomingMessage === "status"){
-        msgToSend = orderData.order[id];
+    else if(incomingMessage.status){
+        const statusId = incomingMessage.status
+        msgToSend = orderData.order[statusId-1];
     } else{
         throw (`${incomingMessage} is not in the meal lists` );
         return;
@@ -40,40 +41,63 @@ router.get("/", (req, res) => {
 // to receive sms
 router.post("/", (req, res) => {
     // look in re.body to see incoming message
-    const incomingMessage = req.body.order;
+    const incomingMessage = req.body;
     const typeValue = ["pizza", "burger", "salad"];
     let msgToSend;
     let idNew;
-    const bool = typeValue.includes(incomingMessage);
-    if(bool){ 
-        //console.log(typeValue.includes(incomingMessage));       
-        const index = orderData.index;
-        //console.log(orderData);
-        const id = index +1;
-        const newOrder = new Order(incomingMessage,id);
-        msgToSend = `Order nr. ${id}`;         
-        orderData.index = index + 1;
-        idNew = id;
-         orderData.order.push(newOrder); 
-         //console.log(newOrder);  
-         const orderNewJson = JSON.stringify(orderData);
-         fs.writeFileSync(
-             __dirname + '/../data/order.json',
-             orderNewJson, (err)=>{
-                 if(err) throw err;
-             }
-        );
-    }else{
+    if(incomingMessage.order){
+        const incomingOrder = incomingMessage.order;
+        const bool = typeValue.includes(incomingOrder);
+        if(bool){ 
+            //console.log(typeValue.includes(incomingOrder));       
+            const index = orderData.index;
+            //console.log(orderData);
+            const id = index +1;
+            const newOrder = new Order(incomingOrder,id);
+            msgToSend = `Order nr. ${id}`;         
+            orderData.index = index + 1;
+            idNew = id;
+            orderData.order.push(newOrder); 
+            //console.log(newOrder);  
+            const orderNewJson = JSON.stringify(orderData);
+            fs.writeFileSync(
+                __dirname + '/../data/order.json',
+                orderNewJson, (err)=>{
+                    if(err) throw err;
+                }
+            );
+        }
+    }    
+    else if(incomingMessage === 'help'){
+        msgToSend = 'Send  menu: to get lists of avialiable meals types' + 
+                        'order(type): to orded what you want ' + 
+                        'status(id): to check status of your order';
+    }
+    else if(incomingMessage === 'menu'){
+        msgToSend = '1:pizza 2:burger 3:salad';
+    }
+    else if(incomingMessage.status){
+        const statusId = incomingMessage.status;
+        console.log(statusId);
+        if(orderData.order[statusId-1]){
+            msgToSend = orderData.order[statusId-1];
+            msgToSend =`Order is ${msgToSend.status}. Last updated ${msgToSend.modified}.`
+        }
+        else
+            msgToSend = 'No order!';
+        
+    } 
+    else{
         throw (`${incomingMessage} is not in the meal lists` );
             return;
     }
     
     // response that you want to send back in response immediately
-   /*const twiml = new MessagingResponse(); 
+   const twiml = new MessagingResponse(); 
    twiml.message(msgToSend);
    res.wrteHead(200, {'Content-Type': 'text/xml'});
-   res.send(twiml.toString());*/
-   res.send(`${msgToSend} is added to order lists with id = ${idNew}!`);
+   res.send(twiml.toString());
+   //res.send(`${msgToSend}`);
 });
 
 module.exports = router;
