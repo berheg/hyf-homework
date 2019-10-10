d50a534697cc030b0a5e5a56753e41255990c6c0
Girmay <ink423103@yahoo.com>
2019-10-10 22:29:41 +0200

    init commit

---
+++ react/week1/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import TaskList from '../src/Components/taskList';
+//import taskObject from "../src/Components/listObject";
+import App from './App';
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App/>, rootElement);
+
