b54befe88637d24a421710a8c617612c209e5a9a
Girmay <ink423103@yahoo.com>
2019-10-16 23:54:00 +0200

    data added as const

--- react/week1/src/App.js
+++ react/week1/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import TaskList from '../src/Components/taskList';
-import taskObject from "../src/Components/listObject";
+//import taskObject from "../src/Components/listObject";
 import './App.css';
 
 function App() {
+  const taskObject =[
+    {
+        description : "Get out of bed",
+        deadline : "Wed Sep 13 2017"
+    },
+    {
+        description : "Brush teeth",
+        deadline : "Thu Sep 14 2017"
+    },
+    {
+        description : "Eat breakfast",
+        deadline : "Fri Sep 15 2017"
+    }
+] 
   return (
     <div className="App">
       <header className="App-header">        
