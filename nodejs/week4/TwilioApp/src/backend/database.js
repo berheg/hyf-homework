417ccd51b301c553e39ff847a32208dee055cdfe
Girmay <ink423103@yahoo.com>
2019-10-24 23:50:15 +0200

    object destructuring used

--- nodejs/week4/TwilioApp/src/backend/database.js
+++ nodejs/week4/TwilioApp/src/backend/database.js
@@ -3,8 +3,6 @@ const Config = require('node-json-db/dist/lib/JsonDBConfig');
 const db = new JsonDB(new Config("myDataBase", true, false, '/'));*/
 require("dotenv").config();
 const port = process.env.PORT||5000;
-const {subdomainKey, accountSid, authToken} = process.env;//.SUBDOMAIN_KEY;
-/*const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;*/
+const {subdomainKey, accountSid, authToken} = process.env;
 
 module.exports = port, subdomainKey, accountSid,authToken;
