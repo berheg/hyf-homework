f412a4694fdc83c3a010bf679e3f76d04a24ee01
Girmay <ink423103@yahoo.com>
2019-11-03 08:41:30 +0100

    component for search input

---
+++ react/week4/usersearcher/src/components/Input.jsx
@@ -0,0 +1,28 @@
+import React,{Component} from 'react';
+
+class Input extends Component {
+  
+  
+  render(){
+    
+  return (
+    <form style={divStyle}>  
+     
+      <input type='text' name = 'userLogin' style={inputStyle} 
+      onChange = {this.props.handleInputChange} 
+      placeholder={"Search for user"}/>  
+    </form>
+  );
+};
+};
+const inputStyle = {
+  boxShadow: '0 0 0 5px orange',
+  alignSelf: 'center'
+
+}
+const divStyle = {  
+  alignSelf: 'center',
+  textAlign: 'center',  
+
+}
+export default Input;
