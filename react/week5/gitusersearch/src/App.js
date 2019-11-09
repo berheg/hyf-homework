2a17fed158a06f1a7d1b0727b48bc86178ef0354
Girmay <ink423103@yahoo.com>
2019-11-09 23:22:39 +0100

    routers are defined

---
+++ react/week5/gitusersearch/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./App.css";
+import Users from "./components/Users";
+import {BrowserRouter as Router, Route } from 'react-router-dom';
+import About from "./components/pages/About";
+import Header from "./components/layout/Header";
+import Overview from "./components/profile/Overview";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">  
+      <Header />
+      <Route
+        exact
+        path= '/githubSearch'
+        render= {(props) =>(
+          <React.Fragment>
+            <Users />
+          </React.Fragment>
+        )}
+        />
+        <Route path='/About' component = {About} />
+        <Route path='/Overview' component = {Overview} />
+        </div>
+    </Router>
+    
+      
+        
+        
+     
+    
+  );
+}
+
+export default App;
+
