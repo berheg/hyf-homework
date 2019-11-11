1a84264bacbc548711a888f4aeb21723f99ef43a
Girmay <ink423103@yahoo.com>
2019-11-11 09:54:56 +0100

    ProfileLink component used

--- react/week5/gitusersearch/src/components/profile/Following.jsx
+++ react/week5/gitusersearch/src/components/profile/Following.jsx
@@ -1,32 +1,12 @@
 import React, {Component} from "react";
-import {Link } from 'react-router-dom';
+import ProfileLink from './ProfileLink';
 
 class Following extends Component {
 
     render(){
         return (
             <React.Fragment>
-               <div style={divStyle}>
-                    <li style={liStyle}>
-                        <Link  to='/Overview'>Overview</Link>
-                    </li>  
-                    <li style={liStyle}>
-                        <Link to='/Repository'>Repository</Link> 
-                    </li>
-                    
-                    <li style={liStyle}>
-                        <Link to='/Stars'>Stars </Link> 
-                    </li>
-                    
-                    <li style={liStyle}>
-                        <Link style={linkStyle} to='/Following'>Following</Link>
-                    </li> 
-                        
-                    <li style={liStyle}>
-                        <Link to='/Followers'>Followers</Link>
-                    </li>   
-                
-                </div>    
+               <ProfileLink />  
               <div className="App">              
               
                   <React.Fragment>
@@ -39,18 +19,6 @@ class Following extends Component {
     }
   
 }
-const divStyle = {
-    display:'flex',
-    flexDirection:'row',
-    alignItem:'Center',
-    textAlign: 'center'
-}
-const liStyle ={
-    textAlign:'center',
-    width:'100px'
-} 
-const linkStyle = {
-    color:'#fff000'
-}
+
 
 export default Following;
