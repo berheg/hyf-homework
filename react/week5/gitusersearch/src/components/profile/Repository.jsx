5e58eab52687474be273c5290602ba88d28794ca
Girmay <ink423103@yahoo.com>
2019-11-11 09:56:23 +0100

    ProfileLink component used

--- react/week5/gitusersearch/src/components/profile/Repository.jsx
+++ react/week5/gitusersearch/src/components/profile/Repository.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Link } from 'react-router-dom';
+import ProfileLink from './ProfileLink';
 
 
 
@@ -8,27 +8,7 @@ class Repository extends Component {
     render(){
         return (
             <React.Fragment>
-               <div style={divStyle}>
-                <li style={liStyle}>
-                    <Link  to='/Overview'>Overview</Link>
-                </li>  
-                <li style={liStyle}>
-                    <Link style={linkStyle} to='/Repository'>Repository</Link> 
-                </li>
-                
-               <li style={liStyle}>
-                <Link to='/Stars'>Stars </Link> 
-                </li>
-               
-               <li style={liStyle}>
-                <Link to='/Following'>Following</Link>
-                </li> 
-                 
-                <li style={liStyle}>
-                    <Link to='/Followers'>Followers</Link>
-                </li>   
-               
-                </div>    
+               <ProfileLink />
               <div >  
                   <React.Fragment>
                    <h1>Repository</h1>
@@ -40,17 +20,5 @@ class Repository extends Component {
     }
   
 } 
-const divStyle = {
-    display:'flex',
-    flexDirection:'row',
-    alignItem:'Center',
-    textAlign: 'center'
-}
-const liStyle ={
-    textAlign:'center',
-    width:'100px'
-}
-const linkStyle = {
-    color:'#fff000'
-}
+
 export default Repository;
