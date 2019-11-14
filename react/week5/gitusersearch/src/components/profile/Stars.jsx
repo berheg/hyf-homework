db7d0ccd4acc2821e08c35727d56a041cc4a5485
Girmay <ink423103@yahoo.com>
2019-11-14 23:00:16 +0100

    accessed data from from link

--- react/week5/gitusersearch/src/components/profile/Stars.jsx
+++ react/week5/gitusersearch/src/components/profile/Stars.jsx
@@ -1,13 +1,14 @@
 import React, {Component} from "react";
 import ProfileLink from './ProfileLink';
 
-
 class Stars extends Component {
 
     render(){
+        const {user} = this.props.location.state;
+        console.log(user);
         return (
             <React.Fragment>
-               <ProfileLink />    
+               <ProfileLink linkStyleStars={linkStyle} user={user}/>    
               <div className="App">  
                   <React.Fragment>
                    <h1>Stars</h1>
@@ -18,5 +19,8 @@ class Stars extends Component {
     }
   
 } 
+const linkStyle = {
+    color:'#fff000'
+}
 
 export default Stars;
