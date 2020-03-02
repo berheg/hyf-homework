e4dc2e29fe14fbbdcc804828553b64e494813f8d
Girmay <ink423103@yahoo.com>
2020-03-02 23:05:13 +0100

    GameViewModel class added

---
+++ DotNETMaster/week3/ThirtyOne.Web/Models/GameViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ThirtyOne.Shared.Models;
+using ThirtyOne.Web.Models;
+
+namespace ThirtyOne.Web.Helpers
+{
+    public class GameViewModel
+    {
+        public Game CurrentGame { get; internal set; }
+        public WebPlayer CurrentPlayer { get; internal set; }
+    }
+}
