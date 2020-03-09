4e54627690e40d8385e404816b3506e6ae3f819e
Girmay <ink423103@yahoo.com>
2020-03-09 20:38:04 +0100

    GameState class added

---
+++ DotNETMaster/week1/ThirtyOne/Models/GameState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirtyOne.Models
+{
+    /// <summary>
+    /// The state a game can exist in
+    /// </summary>
+    public enum GameState
+    {
+        WaitingToStart,
+        InProgress,
+        GameOver
+    }
+}
