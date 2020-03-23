43d075f8a5d49d8495fe0e65f06f28df89dfed25
Girmay <ink423103@yahoo.com>
2020-03-23 21:08:47 +0100

    PlayerAction class added

---
+++ DotNETMaster/week3/ThirtyOne.Shared/Models/PlayerAction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThirtyOne.Shared.Models
+{
+    public enum PlayerAction
+    {
+        Knock,
+        DrawFromDeck,
+        DrawFromTable
+    }
+}
