1d33d18f5b3206a23537f1459a06ac4af796df6b
Girmay <ink423103@yahoo.com>
2020-06-13 15:09:44 +0200

    reviews addded to each meal object

--- nodejs/week1/mealSharing/src/backend/routes/cheap-meals.js
+++ nodejs/week1/mealSharing/src/backend/routes/cheap-meals.js
@@ -5,10 +5,22 @@ const mealsJson = fs.readFileSync (
     __dirname + '/../data/meals.json',
     'utf8'
   );
+  const reviewsJson = fs.readFileSync (
+    __dirname + '/../data/reviews.json',
+    'utf8'
+  );
   const mealsObject = JSON.parse (mealsJson);
+  const reviewsObject = JSON.parse (reviewsJson);
 //Router for cheap meals request
 router.get('/', (req, res) => {    
     const cheapMealsObject = mealsObject.filter(meal => {return meal.price < 100;});
+    cheapMealsObject.forEach(meal => {
+      const reviews = reviewsObject.filter(review =>{
+        return review.mealId = meal.id;
+      })
+      meal.reviews= reviews;
+    });
+    
     res.send(cheapMealsObject);
 }
 );
